7e5f13b27a42879b21785649dee8c7c8b81cb302
Trường Vĩnh Trịnh <48130078+trinhvinhtruong96@users.noreply.github.com>
2024-12-14 14:10:33 +0700

    Initialize a React project

---
+++ src/App.styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyleAppWrapper = styled.div`
+    max-width: 130rem;
+    margin: 22px auto;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {StyleAppWrapper} from "./App.styled";
+
+const App = () => {
+	return (
+		<StyleAppWrapper>
+			<h1>This is TvTReact</h1>
+		</StyleAppWrapper>
+	);
+};
+
+export default App;
